223fd1638bbf715dcc5afc9de1be0f6085b4fb85
furkan32-ai <furkan.ozdemir1@outlook.fr>
2026-06-22 10:22:15 +0200

    Delete django directory

--- django/capteurs_app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'capteurs_app'
-
-urlpatterns = [
-    path('', views.liste_capteurs, name='liste_capteurs'),
-    path('capteur/<str:id_capteur>/', views.detail_capteur, name='detail_capteur'),
-]
