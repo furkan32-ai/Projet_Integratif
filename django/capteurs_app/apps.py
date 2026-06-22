a3bba562354ad67dc0e0b9b0914cf7ce231f18c1
furkan32-ai <furkan.ozdemir1@outlook.fr>
2026-06-22 09:54:56 +0200

    Add files via upload

---
+++ django/capteurs_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class CapteurAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'capteurs_app'
---
+++ django/capteurs_app/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Capteur, Mesure
+
+
+def liste_capteurs(request):
+    pass  # à implémenter
+
+
+def detail_capteur(request, id_capteur):
+    pass  # à implémenter
